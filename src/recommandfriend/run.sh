03303ea658e0ee1731ceae49c40124a626d14220
foalem <fpatrickloic@yahoo.fr>
2022-10-25 15:09:16 -0400

    Setup recommendation package

--- src/recommandfriend/run.sh
+++ src/recommandfriend/run.sh
@@ -9,7 +9,7 @@ echo "delete output if exist directory..."
 hdfs dfs -rm -r output
 echo "Done."
 echo "Running friend recommendation on Hadoop..."
-hadoop jar friends.jar org.myorg.Friends input output
+hadoop jar wc.jar recommendation_friend input output
 echo "Done."
 echo "Getting output...."
 hdfs dfs -copyToLocal -f output
